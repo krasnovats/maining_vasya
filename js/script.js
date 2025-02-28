373ca668f290b2d24b5f6255ca69842a41105c15
Tatyana_Krasnova <krasnova1987art@yandex.ru>
2025-02-28 15:51:02 +0300

    Ваше сообщение о коммите

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ console.log('++++')
 const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 
-menuToggle.addEventListener('click', () => {
-    console.log('Menu toggle clicked'); // Это сообщение должно появляться в консоли
+menuToggle.onclick = () => {
     mobileMenu.classList.toggle('hidden');
-});
+};
 
 // ndsklkl
